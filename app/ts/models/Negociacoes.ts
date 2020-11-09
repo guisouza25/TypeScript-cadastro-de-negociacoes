e5fa1c7c299472cec94b35d831db8e1e2fb7b125
José Guilherme Ferreira de Souza <guilhermefsouza@hotmail.com>
2020-11-09 16:15:41 -0300

    Exibindo negociação na view e utilizando herança

---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,19 @@
+class Negociacoes {
+	      //_negociacoes:Negociacao[] array de negociacao
+	private _negociacoes: Array<Negociacao> = [];
+
+	//encapsular a lista de negociacoes para que seja
+	//possivel apenas incluir e nao remover
+	
+	adiciona(negociacao: Negociacao): void {
+		this._negociacoes.push(negociacao);
+	}
+
+	getNegociacoes(): Array<Negociacao> {
+
+		//passando a lista de negociacoes para um novo array, preservando
+		//o array original(_negociacoes)
+
+		return [].concat(this._negociacoes);
+	}
+}
---
+++ app/ts/views/View.ts
@@ -0,0 +1,17 @@
+class View<T> {
+
+	protected _elemento: Element;
+
+	constructor(seletor: string) {
+		this._elemento = document.querySelector(seletor);
+	}
+
+
+	update(model: T): void {
+		this._elemento.innerHTML = this.template(model);
+	}
+	
+	template(model: T): string {
+		throw new Error('Método deve ser implementado');
+	}
+}
